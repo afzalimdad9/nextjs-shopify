821337ef6bf19b3ce49e30d1e2aa30c27945bacb
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 11:53:25 +0500

    Added back some required React imports

--- components/product/ProductGrid/ProductGrid.tsx
+++ components/product/ProductGrid/ProductGrid.tsx
@@ -12,12 +12,9 @@ const ProductView: FC<Props> = ({ products, className }) => {
   const rootClassName = cn(s.root, className)
   return (
     <div className={rootClassName}>
-      <div></div>
-      <div className="row-span-1 lg:col-span-1 bg-black h-full"></div>
-      <div className="row-span-1 lg:col-span-1 bg-pink"></div>
-      <div className="row-span-1 lg:col-span-1 bg-black h-full"></div>
-      <div className="row-span-2 lg:col-span-2 bg-blue h-full"></div>
-      <div className="row-span-1 lg:col-span-1 bg-cyan"></div>
+      {products.slice(0, 6).map(({ node }) => (
+        <ProductCard productData={node} />
+      ))}
     </div>
   )
 }
