681257bf2f6e534b79489a4add16c8fb585e7c7f
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 12:37:12 +0500

    Correct spelling in readme

--- components/common/UserNav/UserNav.tsx
+++ components/common/UserNav/UserNav.tsx
@@ -1,14 +1,13 @@
 import { FC, useEffect, useState } from 'react'
 import cn from 'classnames'
-import s from './UserNav.module.css'
-import { FC } from 'react'
-import { Heart, Bag } from '@components/icons'
-import { Avatar } from '@components/common'
-import { useUI } from '@components/ui/context'
-import DropdownMenu from './DropdownMenu'
-import { Menu } from '@headlessui/react'
 import useCart from '@bigcommerce/storefront-data-hooks/cart/use-cart'
 import useCustomer from '@bigcommerce/storefront-data-hooks/use-customer'
+import { Heart, Bag } from '@components/icons'
+import { useUI } from '@components/ui/context'
+import DropdownMenu from './DropdownMenu'
+import s from './UserNav.module.css'
+import { Avatar } from '@components/common'
+
 interface Props {
   className?: string
 }
@@ -20,9 +19,9 @@ const countItems = (count: number, items: any[]) =>
 const UserNav: FC<Props> = ({ className, children, ...props }) => {
   const { data } = useCart()
   const { data: customer } = useCustomer()
-
   const { toggleSidebar, closeSidebarIfPresent, openModal } = useUI()
   const itemsCount = Object.values(data?.line_items ?? {}).reduce(countItems, 0)
+
   return (
     <nav className={cn(s.root, className)}>
       <div className={s.mainContainer}>
@@ -40,16 +39,7 @@ const UserNav: FC<Props> = ({ className, children, ...props }) => {
           </li>
           <li className={s.item}>
             {customer ? (
-              <Menu>
-                {({ open }) => (
-                  <>
-                    <Menu.Button className={s.avatarButton} aria-label="Menu">
-                      <Avatar />
-                    </Menu.Button>
-                    <DropdownMenu open={open} />
-                  </>
-                )}
-              </Menu>
+              <DropdownMenu />
             ) : (
               <button
                 className={s.avatarButton}
