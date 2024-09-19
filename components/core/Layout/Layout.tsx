b322cb7c71330c24ad5fc20bf6c7f7e3439b40f7
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 12:02:16 +0500

    Changes to the headings

--- components/core/Layout/Layout.tsx
+++ components/core/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import cn from 'classnames'
 import { ThemeProvider } from 'next-themes'
 import { SSRProvider, OverlayProvider } from 'react-aria'
@@ -6,6 +6,7 @@ import type { Page } from '@lib/bigcommerce/api/operations/get-all-pages'
 import { CommerceProvider } from '@lib/bigcommerce'
 import { Navbar, Featurebar, Footer } from '@components/core'
 import { Container, Sidebar } from '@components/ui'
+import Button from '@components/ui/Button'
 import { CartSidebarView } from '@components/cart'
 import { useUI } from '@components/ui/context'
 
@@ -18,22 +19,31 @@ interface Props {
 const Layout: FC<Props> = ({ className, children }) => {
   const rootClassName = cn(s.root, className)
   const { displaySidebar, closeSidebar } = useUI()
+  const [acceptedCookies, setAcceptedCookies] = useState(false)
 
   return (
-    <div className="h-full bg-primary">
-      <Featurebar
-        title="Free Standard Shipping on orders over $99.99"
-        description="Due to COVID-19, some orders may experience processing and delivery delays."
-      />
-      <Container>
-        <Navbar />
-      </Container>
-      <main className="fit">{children}</main>
-      <Footer pages={pages} />
-      <Sidebar show={displaySidebar} close={closeSidebar}>
-        <CartSidebarView />
-      </Sidebar>
-    </div>
+    <CommerceProvider locale="en-us">
+      <div className={cn(s.root)}>
+        <Container>
+          <Navbar />
+        </Container>
+        <main className="fit">{children}</main>
+        <Footer pages={pageProps.pages} />
+        <Sidebar show={displaySidebar} close={closeSidebar}>
+          <CartSidebarView />
+        </Sidebar>
+        <Featurebar
+          title="This site uses cookies to improve your experience."
+          description="By clicking, you agree to our Privacy Policy."
+          action={
+            <Button className="mx-5" onClick={() => setAcceptedCookies(true)}>
+              Accept cookies
+            </Button>
+          }
+          className={cn({ 'translate-y-full': acceptedCookies })}
+        />
+      </div>
+    </CommerceProvider>
   )
 }
 
