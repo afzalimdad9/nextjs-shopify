edd4f6c075689906a54286b929885fa3cca2d25e
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 11:48:24 +0500

    Arch and components by functionality

--- components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Avatar } from "./Avatar";
-export { default as Footer } from "./Footer";
-export { default as Navbar } from "./Navbar";
-export { default as Searchbar } from "./Searchbar";
-export { default as ProductView } from "./ProductView";
-export { default as Layout } from "./Layout";
-export { default as Featurebar } from "./Featurebar";
