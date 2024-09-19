7bbb6e2d3346f54eea9c1809639aade86fcea1d6
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 12:50:59 +0500

    cart upsell, announcement bar and targeting by items in cart

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,4 +1,4 @@
-import type { GetStaticPropsContext } from 'next'
+import { } from 'react'
 import { useCart, useCheckoutUrl } from '@lib/shopify/storefront-data-hooks'
 import { Layout } from '@components/common'
 import { Button } from '@components/ui'
@@ -7,6 +7,8 @@ import useCart from '@bigcommerce/storefront-data-hooks/cart/use-cart'
 import usePrice from '@bigcommerce/storefront-data-hooks/use-price'
 import { CartItem } from '@components/cart'
 import { Text } from '@components/ui'
+import NoSSR from '@components/common/NoSSR/NoSSR'
+import { BuilderComponent } from '@builder.io/react'
 
 export default function Cart() {
   const cart = useCart()
@@ -15,9 +17,6 @@ export default function Cart() {
   const total = ' - '
   const items = cart?.lineItems ?? []
   const isEmpty = items.length === 0
-
-  const error = null
-  const success = null
 
   return (
     <div className="grid lg:grid-cols-12">
@@ -33,25 +32,6 @@ export default function Cart() {
             <p className="text-accents-6 px-10 text-center pt-2">
               Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
             </p>
-          </div>
-        ) : error ? (
-          <div className="flex-1 px-4 flex flex-col justify-center items-center">
-            <span className="border border-white rounded-full flex items-center justify-center w-16 h-16">
-              <Cross width={24} height={24} />
-            </span>
-            <h2 className="pt-6 text-xl font-light text-center">
-              We couldn’t process the purchase. Please check your card
-              information and try again.
-            </h2>
-          </div>
-        ) : success ? (
-          <div className="flex-1 px-4 flex flex-col justify-center items-center">
-            <span className="border border-white rounded-full flex items-center justify-center w-16 h-16">
-              <Check />
-            </span>
-            <h2 className="pt-6 text-xl font-light text-center">
-              Thank you for your order.
-            </h2>
           </div>
         ) : (
           <div className="px-4 sm:px-6 flex-1">
