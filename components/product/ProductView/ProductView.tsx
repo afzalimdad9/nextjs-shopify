b75bd548760e002f14968958deb418ba72c2759a
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 12:16:20 +0500

    a11y pass

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -81,6 +81,7 @@ const ProductView: FC<Props> = ({ product, className }) => {
               {product.images.edges?.map((image, i) => (
                 <div key={image?.node.urlXL} className={s.imageContainer}>
                   <Image
+                    alt={product.name}
                     className={s.img}
                     src={image?.node.urlXL!}
                     width={1050}
@@ -132,6 +133,7 @@ const ProductView: FC<Props> = ({ product, className }) => {
           </section>
           <div>
             <Button
+              aria-label="Add to Cart"
               type="button"
               className={s.button}
               onClick={addToCart}
