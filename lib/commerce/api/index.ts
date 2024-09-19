44a03d0e6bce2d67c7d965ef1793dc9765ff8a4d
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 11:48:57 +0500

    Simplify configs

--- lib/commerce/api/index.ts
+++ lib/commerce/api/index.ts
@@ -1,20 +1,5 @@
-export interface CommerceAPIOptions {
-  commerceUrl: string;
-  apiToken: string;
-}
-
-export interface CommerceAPIFetchOptions {
-  variables?: object;
-  preview?: boolean;
-}
-
 export interface CommerceAPI {
-  commerceUrl: string;
-  apiToken: string;
-
-  fetch<T>(query: string, queryData?: CommerceAPIFetchOptions): Promise<T>;
-
-  getAllProducts(options?: { query?: string }): Promise<any>;
+  getAllProducts(query: string): Promise<any>;
 }
 
 // export default class CommerceAPI {
