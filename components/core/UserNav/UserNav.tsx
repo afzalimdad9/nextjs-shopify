e63259ed92c2bc685a3914da5b023d9d5047a3eb
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-19 12:16:24 +0500

    a11y pass

--- components/core/UserNav/UserNav.tsx
+++ components/core/UserNav/UserNav.tsx
@@ -65,7 +65,7 @@ const UserNav: FC<Props> = ({ className, children, ...props }) => {
             <Menu>
               {({ open }) => (
                 <>
-                  <Menu.Button className={s.avatarButton}>
+                  <Menu.Button className={s.avatarButton} aria-label="Menu">
                     <Avatar />
                   </Menu.Button>
                   <DropdownMenu open={open} />
